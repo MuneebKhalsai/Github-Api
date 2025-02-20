b56aead27bec650de203bc6a34f5857a8cc7d40c
Muneeb Khalsai <muneeb.akhlaq05@gmail.com>
2025-02-21 00:06:14 +0500

    Assignment complete

---
+++ app.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form');
+const id = document.getElementById('id');
+    const div = document.querySelector('div');
+form.addEventListener('submit', (event) =>{
+    event.preventDefault();
+
+    const userId = id.value.trim();
+axios(`https://api.github.com/users/${userId}`)
+.then(res => {
+    div.innerHTML = "";
+    const data = res.data;
+    div.innerHTML += `
+    <div class = "card">
+    <img src = '${data.avatar_url}' width = '240px'>
+    <br>
+    Name: ${data.name};
+    <br>
+    Bio: ${data.bio};
+    <br>
+    Id: ${data.id};
+    <br>
+    Email:${data.email};
+    <br>
+    </div>
+    `;
+})
+.catch(err => {
+    console.log('Error' , err);
+    alert('Github Username Is Invalid');
+});
+});
+
+
+
+
+ // Extra spaces remove karega
+
+    // if (!userId) {
+    //     alert('Please enter a valid GitHub username');
+    //     return;
+    // }
+
